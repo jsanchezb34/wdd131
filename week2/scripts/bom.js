6d8b799b691025e692315c74075c1b77035e3e54
jsanchezb34 <jsanchezb34@>
2024-06-22 23:20:12 -0400

    first

---
+++ week2/scripts/bom.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("favchap");
+const button = document.querySelector ('button');
+const lisr = document.querySelector ('bom.html');
+
+const li = document.createElement ('li');
+const deletebutton = document.createElement ('button');
+li.textContent = input.value;
+
+deletebutton.textContent = '❌' 
+li.append (deletebutton;)
