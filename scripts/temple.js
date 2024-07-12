01dd89f28929d2c617d0f60bc7c8f02777efaee4
jsanchezb34 <jsanchezb34@>
2024-07-11 22:37:54 -0400

    temples addition

--- scripts/temple.js
+++ scripts/temple.js
@@ -55,7 +55,31 @@ const temples = [
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
     },
-    // Add more temple objects here...
+    {
+    templeName: "Cochabamba Bolivia Temple",
+      location: "Cochabamba, Bolivia",
+      dedicated: "2000, April, 30",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+          location: "Mexico City, Mexico",
+          dedicated: "1983, December, 2",
+          area: 116642,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+    templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    
   ];
 
   function displayTemples(temples) {
